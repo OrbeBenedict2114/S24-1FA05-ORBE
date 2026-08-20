4fc67fe6101b14ade762caeadcff455ef9f722a8
OrbeBenedict2114 <bsorbe@cmc.pshs.edu.ph>
2026-08-20 20:31:57 +0800

    Add files via upload

---
+++ S24-1FA05-ORBE.py
@@ -0,0 +1,16 @@
+# REFLECTION:
+# Using a library is more practical than writing calculations from scratch because it saves time, 
+# prevents errors, and optimizes performance with pre-tested functions. For example, instead of 
+# writing a complex algorithm to manually calculate a square root, importing math.sqrt() allows us 
+# to compute it reliably in a single line of code.
+
+import math
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+
+print(f"\nThe distance between the two points is: {distance:.2f}")
