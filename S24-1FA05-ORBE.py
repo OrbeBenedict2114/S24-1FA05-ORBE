e6ce9c058b625c4dfec6036ab1cb6fee274eda5c
OrbeBenedict2114 <bsorbe@cmc.pshs.edu.ph>
2026-09-15 08:30:45 +0800

    Added Meaningful Comments to the Code
    Added comments for clarity and structure in the code.

--- S24-1FA05-ORBE.py
+++ S24-1FA05-ORBE.py
@@ -5,12 +5,12 @@
 # to compute it reliably in a single line of code.
 
 import math
-
+# input the 2 points on the 2d plane
 x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
-
+# The Formula/Equation
 distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
-
+# The Final Answer/The distance between both points
 print(f"\nThe distance between the two points is: {distance:.2f}")
